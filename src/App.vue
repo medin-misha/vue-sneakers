5bb93842b978ff66a68e1df2927459be92fabb99
misha <medinskijmisa228@gmail.com>
2025-01-01 19:51:59 +0200

    learn vue

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import CardComponent from './components/CardComponent.vue'
+import HeaderComponent from './components/HeaderComponent.vue'
+import SliderComponent from './components/SliderComponent.vue'
+import ProductsComponent from './components/ProductsComponent.vue'
+</script>
+
+<template>
+  <div class="
+  w-11/12 h-screen
+  m-auto p-3 my-8
+  bg-slate-300
+  rounded-xl
+  shadow-2xl">
+    <HeaderComponent />
+    <SliderComponent />
+    <ProductsComponent />
+    <CardComponent />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+
+<template>
+ <header class="border-slate-400 border-b flex justify-between">
+  <div class="flex items-center gap-4 px-10 py-6">
+    <img src="/logo.png" alt="лого" class="w-10"/>
+    <div>
+      <h2 class="text-xl font-bold">Sneakers</h2>
+      <p class="text-slate-500">Ахуенные кросовки!</p>
+    </div>
+  </div>
+  <div>
+    <ul class="cursor-pointer flex items-center gap-10 py-8 px-10">
+      <li class="flex item-center gap-2">
+        <img src="/cart.svg" alt="стоимость корзины">
+        <span class="text-gray-500 hover:text-gray-600">Стоимость корзины: 123.2</span>
+      </li>
+      <li class="cursor-pointer flex item-center gap-2">
+        <img src="/heart.svg" alt="вкладки">
+        <span class="text-gray-500 hover:text-gray-600">Закладки</span>
+      </li>
+      <li class="cursor-pointer flex item-center gap-2">
+        <img src="/profile.svg" alt="профиль">
+        <span class="text-gray-500 hover:text-gray-600">Профиль</span>
+      </li>
+    </ul>
+  </div>
+ </header>
+</template>
+
+<style scoped>
+
+</style>
